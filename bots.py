53fb6d035c9d61d48077d3cfc1d291d955e559cb
PRANKBOTS <33993971+prankbots@users.noreply.github.com>
2019-05-27 01:45:12 +0700

    Update bots.py

--- bots.py
+++ bots.py
@@ -2,6 +2,8 @@ from bot import OverPoll
 import threading
 def login(resp, auth):
     bot = OverPoll(resp, auth)
-    
-threading.Thread(target=login, args=("client","token")).start()
+
+threading.Thread(target=login, args=("client1","token")).start()
+threading.Thread(target=login, args=("client2","token")).start()
+threading.Thread(target=login, args=("client3","token")).start()
 print("USER LOGIN SUCCES.!!")
